c4e141e96b2615d954f41d72b97ac515904336dc
rubenhamza1 <90246098+rubenhamza1@users.noreply.github.com>
2022-02-15 13:08:08 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vježba2
+{
+    class ProgramskiJezik
+    {
+        double postotak;
+
+        public double Postotak { get => postotak; set => postotak = value; }
+    }
+
+    class Proceduralni : ProgramskiJezik
+    {
+
+    }
+
+    class Objektni : ProgramskiJezik
+    {
+
+    }
+
+    class Funkcionalni : ProgramskiJezik
+    {
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Proceduralni x = new Proceduralni();
+            Objektni y = new Objektni();
+            Funkcionalni z = new Funkcionalni();
+
+            Console.WriteLine("Unesi vrijednost postotka za x: ");
+            int p = Convert.ToInt32(Console.ReadLine());
+            x.Postotak = p;
+            Console.WriteLine("Unesi vrijednost postotka za y: ");
+            y.Postotak = p;
+            Console.WriteLine("Unesi vrijednost postotka za z: ");
+            z.Postotak = p;
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+    
+}
